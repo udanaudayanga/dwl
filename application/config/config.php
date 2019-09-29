d66f048cdeb7ae20590500b1b3037ef3e83d8ca6
Udana Udayanga <udanaudayanga@gmail.com>
2019-09-29 14:13:05 +0530

    localhost update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://uncle.localhost/';
+$config['base_url'] = 'http://dwl.localhost/';
 
 /*
 |--------------------------------------------------------------------------
